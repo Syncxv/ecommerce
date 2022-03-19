28432bffa9ce8ffd8afcc56722a4e19ef408039f
Syncxv <aruldevarackal1@gmail.com>
2022-03-20 01:42:12 +0530

    init

---
+++ app/root.tsx
@@ -0,0 +1,48 @@
+import { Links, LinksFunction, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "remix";
+import type { MetaFunction } from "remix";
+
+export const meta: MetaFunction = () => {
+    return { title: "New Remix App" };
+};
+
+// export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+export default function App() {
+    return (
+        <Document>
+            <Layout>
+                <Outlet />
+            </Layout>
+        </Document>
+    );
+}
+
+export const Document: React.FC = ({ children }) => {
+    return (
+        <html lang="en">
+            <head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width,initial-scale=1" />
+                <title>Hi</title>
+                <Meta />
+                <Links />
+            </head>
+            <body>
+                {children}
+                <ScrollRestoration />
+                <Scripts />
+                {process.env.NODE_ENV === "development" && <LiveReload />}
+            </body>
+        </html>
+    );
+};
+
+export const Layout: React.FC = ({ children }) => {
+    return (
+        <>
+            <div className="hehhe">
+                hi there
+                <main>{children}</main>
+            </div>
+        </>
+    );
+};
