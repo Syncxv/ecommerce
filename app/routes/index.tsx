e43bcf0e38f4091af5ff535af63918ed3951f8c5
Syncxv <aruldevarackal1@gmail.com>
2022-03-20 02:40:06 +0530

    woah

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,15 @@
+import { useLoaderData } from "remix";
+import { db } from "~/utils/db.server";
+
+export const loader = async () => {
+    console.log(db);
+    const test = db.TEST.findMany();
+    console.log(test);
+    return { hi: "hehe", test: test || "bruh" };
+};
 export default function Index() {
+    const hi = useLoaderData();
+    console.log(hi);
     return (
         <div>
             <h1>hi</h1>
