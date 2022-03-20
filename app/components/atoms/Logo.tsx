538a256118813396d6193e2084beacc5aa2d5e97
Syncxv <aruldevarackal1@gmail.com>
2022-03-20 10:36:45 +0530

    LOGO

---
+++ app/components/atoms/Logo.tsx
@@ -0,0 +1,14 @@
+interface Props {}
+
+const Logo: React.FC = ({}) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 70 70" fill="none">
+            <rect x="26" y="24" width="4" height="4" rx="1.5" fill="black" />
+            <rect x="25" y="42" width="4" height="4" rx="1.5" fill="black" />
+            <rect x="42" y="24" width="4" height="4" rx="1.5" fill="black" />
+            <rect x="41" y="42" width="4" height="4" rx="1.5" fill="black" />
+        </svg>
+    );
+};
+
+export default Logo;
